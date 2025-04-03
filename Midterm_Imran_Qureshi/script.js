c34a3d0ab8e245abaed3bdc52d98ca67f76282db
Imran946386 <iqureshi1@pride.hofstra.edu>
2025-04-03 00:59:32 -0400

    Initial Final Version

--- Midterm_Imran_Qureshi/script.js
+++ Midterm_Imran_Qureshi/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const colors = ["#D40000", "#545b62", "#E66b00"]; // Rosso Corsa(Red), Gray Titanium, Giallo Modena(Yellow)
+    const colors = ["#D40000", "#545b62", "#F6F305"]; // Rosso Corsa(Red), Gray Titanium, Giallo Modena(Yellow)
     console.log("The first color in the array is:", colors[0]);
     const userName = prompt("What is your name?");
     const colorChoice = prompt("Pick a background color: 0 for Red, 1 for Gray, 2 for Yellow");
