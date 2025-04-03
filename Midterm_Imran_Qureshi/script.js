b76892e639df0ec27d400698286a4e37d4a893c0
Imran946386 <iqureshi1@pride.hofstra.edu>
2025-04-03 15:22:49 -0400

    Final commit message

--- Midterm_Imran_Qureshi/script.js
+++ Midterm_Imran_Qureshi/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // Event lister for Document Object Model when loading page
     const colors = ["#D40000", "#545b62", "#F6F305"]; // Rosso Corsa(Red), Gray Titanium, Giallo Modena(Yellow)
     console.log("The first color in the array is:", colors[0]);
     const userName = prompt("What is your name?");
